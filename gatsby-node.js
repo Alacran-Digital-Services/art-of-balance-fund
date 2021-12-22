26df40af3c97fc6e0cc6aa94b94ba89622fb8b6f
Jorge Viramontes <>
2021-12-22 15:17:51 -0600

    Fix console log warnings: https://robertmarshall.dev/blog/fix-warn-chunk-commons-mini-css-extract-plugin-error-in-gatsby-js/

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,1 +1,12 @@
-// Not needed at this time.
+const FilterWarningsPlugin = require("webpack-filter-warnings-plugin");
+
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    plugins: [
+      new FilterWarningsPlugin({
+        exclude:
+          /mini-css-extract-plugin[^]*Conflicting order. Following module has been added:/,
+      }),
+    ],
+  });
+};
