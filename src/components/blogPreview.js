d4bfe077abf15c7c75fc8f4d9e04c9a5d4ad191a
Amber Abreu <ambermabreu@gmail.com>
2021-12-29 11:08:01 -0500

    Successful Notion Integration

---
+++ src/components/blogPreview.js
@@ -0,0 +1,37 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import * as styles from "../components/blogPreview.module.css"
+import CommonButton from "./common/commonButton"
+
+const BlogPreview = ({ blogTitle, excerpt, date, id, imgUrl }) => (
+  <div className={styles.blog}>
+    <div className={styles.description}>
+      <p>{date}</p>
+      <img src={imgUrl} alt="blog image" />
+      <h2>
+        <Link to={id} className={styles.title}>
+          {blogTitle}
+        </Link>
+      </h2>
+      <p>{excerpt}</p>
+      <CommonButton
+        buttonTitle="Read more"
+        slug={id}
+        isGatsbyLink={true}
+      ></CommonButton>
+    </div>
+  </div>
+)
+
+BlogPreview.propTypes = {
+  blogTitle: PropTypes.string,
+  excerpt: PropTypes.string,
+}
+
+BlogPreview.defaultProps = {
+  blogTitle: ``,
+  excerpt: ``,
+}
+
+export default BlogPreview
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,81 @@
 import * as React from "react"
-
+import { useStaticQuery, graphql, Link } from "gatsby"
 import "../styles/global.css"
 import "../styles/index.css"
 
 import HomeHeader from "../components/homeHeader"
+import BlogPreview from "../components/blogPreview"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query MyQuery {
+      allNotion {
+        nodes {
+          id
+          title
+          properties {
+            blurb {
+              value
+            }
+            body {
+              value
+            }
+            coverImageUrl {
+              value
+            }
+            date {
+              value {
+                start(formatString: "MM/DD/yyy")
+              }
+            }
+            subtitle {
+              value
+            }
+          }
+        }
+      }
+    }
+  `)
 
-    <HomeHeader></HomeHeader>
+  const blogPreviews = data.allNotion.nodes.map(node => (
+    <BlogPreview
+      key={node.id}
+      imgUrl={node.properties.coverImageUrl.value}
+      blogTitle={node.title || `Blog Title`}
+      excerpt={
+        node.properties.subtitle.value || `Short description of the blog.`
+      }
+      date={node.date}
+      // slug={node.slug}
+    />
+  ))
 
-    <div className="copyContent">
-      <h1 className="secondary">Our Mission</h1>
-      <p>
-        This is placeholder text. Place your mission here.
-      </p>
-    </div>
+  return (
+    <Layout>
+      <Seo title="Home" />
 
-    <div className="copyContent">
-      <h1 className="primary">Our Values</h1>
-      <p>
-        This is placeholder text. Place your values here.
-      </p>
-    </div>
+      <HomeHeader></HomeHeader>
 
-    <div className="copyContent">
-      <h1 className="secondary">Community Engagement</h1>
-      <p>
-        This is placeholder text. Incorporate a blog here.
-      </p>
-    </div>
-  </Layout>
-)
+      <div className="copyContent">
+        <h1 className="secondary">Our Mission</h1>
+        <p>This is placeholder text. Place your mission here.</p>
+      </div>
 
+      <div className="copyContent">
+        <h1 className="primary">Our Values</h1>
+        <p>This is placeholder text. Place your values here.</p>
+      </div>
+
+      <div className="copyContent">
+        <h1 className="secondary">Community Engagement</h1>
+        <div className="standardSpacing">
+          <Seo title="Blog" />
+          <div> {blogPreviews} </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
 export default IndexPage
