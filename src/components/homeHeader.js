35d80202d2ccce3d201ac6f5b68bee066af8b410
Amber Abreu <ambermabreu@gmail.com>
2022-01-26 15:31:11 -0500

    Updated Donation Slider with Text Input and Helper Texts

--- src/components/homeHeader.js
+++ src/components/homeHeader.js
@@ -1,51 +1,55 @@
-
-import React from 'react'
-import { homeBanner, bannerDetails, title, tagline } from "./homeHeader.module.css"
+import React from "react"
+import {
+  homeBanner,
+  bannerDetails,
+  title,
+  tagline,
+} from "./homeHeader.module.css"
 import { StaticImage } from "gatsby-plugin-image"
 import SocialLinks from "../components/socialLinks"
-import DonationForm from './forms/donationForm'
+import DonationForm from "./forms/donationForm"
 
 const homeHeader = () => {
-    return (
-        <div className={homeBanner}>
-            <div className={bannerDetails}>
-                <div className={title}>
-                    Art of Balance Fund
-                </div>
+  return (
+    <div className={homeBanner}>
+      <div className={bannerDetails}>
+        <div className={title}>Art of Balance Fund</div>
 
-                <h4 className={tagline}>
-                    Helping make mental health accessible to all.
-                </h4>
+        <h4 className={tagline}>
+          Helping make mental health accessible to all.
+        </h4>
 
-                <div>
-                    <p>
-                        We are a not-for-profit organization generating grants for mental health.
-                        We want to decrease stigma and increase awareness of mental health needs. 
-                        We aim to educate corporate offices to increase awareness for love, care and affection everywhere you go. 
-                        We advocate for public policies lobbying for mental health care. 
-                        Our mission is to provide accessible mental health for everyone.     
-                    </p>
+        <div>
+          <p>
+            We are a not-for-profit organization generating grants for mental
+            health. We want to decrease stigma and increase awareness of mental
+            health needs. We aim to educate corporate offices to increase
+            awareness for love, care and affection everywhere you go. We
+            advocate for public policies lobbying for mental health care. Our
+            mission is to provide accessible mental health for everyone.
+          </p>
 
-                    <DonationForm></DonationForm>
-                </div>
-
-                <SocialLinks></SocialLinks>
-            </div>
-
-            <div>
-                <StaticImage
-                    src="../images/art-of-balance.png" alt="Art of Balance Logo"
-                    placeholder="blurred"
-                    layout="fixed"
-                    width={340}
-                    formats={["AUTO", "WEBP", "AVIF"]}
-                    style={{
-                        margin: `auto auto 1.45rem`
-                    }}
-                />
-            </div>
+          <DonationForm></DonationForm>
         </div>
-    )
+
+        <SocialLinks></SocialLinks>
+      </div>
+
+      <div>
+        <StaticImage
+          src="../images/art-of-balance.png"
+          alt="Art of Balance Logo"
+          placeholder="blurred"
+          layout="fixed"
+          width={340}
+          formats={["AUTO", "WEBP", "AVIF"]}
+          style={{
+            margin: `auto auto 1.45rem`,
+          }}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default homeHeader
