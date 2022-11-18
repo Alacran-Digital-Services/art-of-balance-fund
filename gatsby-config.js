b9386badb1f1e192691f52b4b8cfc745b54da39c
Jorge Viramontes <>
2022-11-18 12:28:48 -0600

    Update dependencies; remove unnecessary plugin.

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,8 +55,7 @@ module.exports = {
         path: `${__dirname}/src/images`,
         name: "images",
       },
-    },
-    `gatsby-plugin-breakpoints`,
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
